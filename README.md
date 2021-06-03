b266821620765fd511d2e2f139a24dfc101febd8
NatalyaDashkovska <dashkovsk@gmail.com>
2021-06-03 23:25:25 +0300

    done

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # goit-node-hw01-CLI
+
+1
+https://ibb.co/MPrzb1L
+
+2
+https://ibb.co/G3Vtzht
+
+3
+https://ibb.co/yqG1rX1
+
+4
+https://ibb.co/By3ds8G
--- contacts.js
+++ contacts.js
@@ -1,7 +1,7 @@
 // contacts.js
 const fs = require("fs");
 const path = require("path");
-
+const { v4: uuidv4 } = require("uuid");
 // console.log(__dirname);
 // console.log(__filename);
 
@@ -41,20 +41,43 @@ function getContactById(contactId) {
 function removeContact(contactId) {
   fs.readFile(contactsPath, (err, data) => {
     try {
-      console.log(data);
+      const contacts = JSON.parse(data);
+      const newContacts = contacts.filter(
+        (item) => String(item.id) !== String(contactId)
+      );
+      const newJSONContacts = JSON.stringify(newContacts);
+      fs.writeFile(contactsPath, newJSONContacts, (err) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+      });
+      console.table(newContacts);
     } catch (err) {
       console.log(err);
     }
   });
 }
 
-function addContact(_name, _email, _phone) {
+function addContact(name, email, phone) {
   fs.readFile(contactsPath, (err, data) => {
-    if (!fs.existsSync("./temp")) {
-      fs.mkdirSync("./temp");
-    }
     try {
-      console.log(data);
+      const contacts = JSON.parse(data);
+      const newContact = {
+        id: uuidv4(),
+        name,
+        email,
+        phone,
+      };
+      const newContacts = [...contacts, newContact];
+      const newJSONContacts = JSON.stringify(newContacts);
+      fs.writeFile(contactsPath, newJSONContacts, (err) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+      });
+      console.table(newContacts);
     } catch (err) {
       console.log(err);
     }
